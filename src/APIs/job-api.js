03e8e1c69cfe31a3ac2472a8628cf26d042d3a6c
abhishek17551 <ababhishek113@gmail.com>
2024-09-24 22:44:47 +0530

    added queries for filtering jobs

--- src/APIs/job-api.js
+++ src/APIs/job-api.js
@@ -1,10 +1,24 @@
 import supabaseClient from "@/utils/supabase";
 
 
-export async function getJobs(token) {
+export async function getJobs(token, { location, company_id, searchQuery }) {
     const supabase = await supabaseClient(token)
 
     let query = supabase.from('jobs').select('*')
+
+    //Queries for filtering jobs
+    if (location) {
+        query = query.eq("location", location);
+      }
+    
+      if (company_id) {
+        query = query.eq("company_id", company_id);
+      }
+    
+      if (searchQuery) {
+        query = query.ilike("title", `%${searchQuery}%`);
+      }
+    
 
     const {data,error} = await query;
     
