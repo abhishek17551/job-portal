bd7ec5e67c9c42cf1ebde7c5589f8bebd0f176a1
abhishek17551 <ababhishek113@gmail.com>
2024-10-28 20:40:43 +0530

    created the JobPage component

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { getSingleJob } from '@/APIs/job-api'
+import useFetch from '@/hooks/useFetch'
+import { useUser } from '@clerk/clerk-react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { BarLoader } from 'react-spinners'
 
-const Job = () => {
+const JobPage = () => {
+  const {isLoaded,user} = useUser()
+  const {id} = useParams()
+
+  const {
+    fn:fnJob,
+    data:job,
+    loading:loadingJob
+  } = useFetch(getSingleJob,{job_id:id})
+
+  
+  useEffect(() => {
+    if(isLoaded) fnJob()
+  },[isLoaded])
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  console.log(job?.title)
   return (
-    <div>Job</div>
+    <div className="flex flex-col gap-8 mt-5">
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">{job?.title}</h1>
+        <img src={job?.company?.logo_url} className='h-14' alt={job?.title}/>
+      </div>
+    </div>
   )
 }
 
-export default Job
+export default JobPage
